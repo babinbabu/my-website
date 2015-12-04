3b2865f5c929a7dfc8d08a96326d102bed85e22b
unknown <babin.babu@polus153.polusindia.com>
2015-12-04 12:45:20 +0530

    latest file

---
+++ index.php
@@ -0,0 +1,203 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <title>Babin Babu - Software Engineer</title>
+
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="css/styles.css" >
+    <link rel="stylesheet" href="css/font-awesome.min.css" >
+    <link rel="stylesheet" href="css/custombox.css" >
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53633940-2', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </head>
+  <body>
+
+
+    <!--<div class="side-panel">
+       <h1>Babin's</h1>
+      <h2>Portfolio Page</h2>
+    </div> -->
+    <div class="side-menu">
+        <button type="button" class="menu-bars js-btnmenu">
+          <span class="bars"></span>
+          <span class="bars"></span>
+          <span class="bars"></span>
+          <span class="bars"></span>
+        </button>
+        <div class="the-menu">
+            <img src="images/babinbabu.jpg" alt="Babin Babu" class="img-circle user-img" /> <!--- User Icon/Image -->
+            <a class="my-name" href="#">Babin Babu</a> <!--- User Name -->
+            <ul class="nav navmenu-nav"> <!--- Menu -->
+              <li><a href="#" class="home">Home</a></li>
+              <li><a href="#">Works</a></li>
+              <li><a href="#" class="js-contact-me" id="contactme">Contact Me</a></li>
+            </ul><!--- End Menu -->
+
+
+            <div class="social-links">
+              <ul>
+                <li><a href="#" class="tw"><i class="fa fa-twitter" title="my twitter"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/babinbabu" class="in" target="_new"><i class="fa fa-linkedin" title="my linkedin"></i></a></li>
+                <li><a href="https://github.com/babinbabu/" class="gh" target="_new"><i class="fa fa-github-alt" title="my github" ></i></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="contact-form">
+            
+          </div>
+    </div>
+    <div class="main-banner">
+      <div class="over-lay"></div>
+
+      <div class="container">        
+        <h1>I'm Babin Babu,</h1>
+        <h2>and I<span class="malarkey"></span><span class="cursor">|</span></h2>
+        <p class="my-portfolio">This is my portfolio page, scroll down to see my works.</p>
+        <div class="arrow animated bounce"></div>
+      </div>
+    </div>
+    <!--  content-body  -->
+    <div class="content-body">
+      <div class="container">
+        <h2>Bio</h2>
+        <h3>Software Engineer <span class="red"> . </span> ASP.Net</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p>
+         It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <a href="resume/cv-babin-babu.en.pdf" download="cv-babin-babu.en.pdf" class="btn download-cv" >Download MY CV</a>
+      </div>
+    </div>
+
+    <!--  portfolio  -->
+    <div class="content-body last-child">
+      <div class="container">
+        <h2>Portfolio</h2>
+        <h3>Look at my <a target="_blank" href="https://in.linkedin.com/in/babinbabu" class="red">LinkedIn</a> profile to know how my skills are rated others.</h3>
+        <p>
+          I make you stand out from the crowd by creating innovative website. My recipe is simple: trendy design and quality coding seasoned with lots of color harmony, because that gives the real flair.
+        </p>
+        <div class="work">
+          <img src="images/portfolio/bakeeth.png" />
+          <div class="desc">
+            <h4 class="red">Bakeeth</h4>
+            <h5 class="red">Technologies</h5>
+            <p>Umbraco,Entity frame work,Jquery</p>
+            <h5 class="red">Description</h5>            
+            <p>
+              Its a web application to manage the profit calculation and manage all web site dynamically with your interface. Its include multiple languages English and Arabic
+            </p>
+            <a class="view-details" href="#">+</a>
+          </div>
+        </div>
+        <div class="work">
+          <img src="images/portfolio/aufgc.png" />
+          <div class="desc">
+            <h4 class="red">AUFGC</h4>
+            <h5 class="red">Technologies</h5>
+            <p>Umbraco,Entity frame work,HTML,Jquery</p>
+            <h5 class="red">Description</h5>            
+            <p>
+              Its a web application to manage the manage all web site dynamically with your interface. Its include multiple languages English and Arabic, its include both career and vendor portal thats done in entity frame work.
+            </p>
+            <a class="view-details" href="#">+</a>
+          </div>
+        </div>
+      
+      </div>
+    </div>
+    <!-- footer  -->
+    <div class="footer">
+      <div class="container">
+        <?php echo date("Y"); ?> Copy right &copy; All right reserved.
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <script src="js/malarkey.min.js"></script>
+      <script src="js/custombox.js"></script>
+
+    <script type="text/javascript">
+        var elem = document.querySelectorAll('.malarkey')[0];
+        var opts = {
+          typeSpeed: 50,
+          deleteSpeed: 50,
+          pauseDelay: 2000,
+          loop: true,
+          postfix: ''
+        };
+        malarkey(elem, opts).type('\'m a  back end developer.')   .pause().delete()
+                            .type(' learn new things everyday.').pause().delete()
+                            .type(' love Jquery, =)').pause().delete();
+
+    </script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $('.menu-bars').bind('click',function(e) {
+          e.preventDefault();
+          $('.side-menu').toggleClass('open');
+        });
+        $('.js-contact-me').bind('click',function(e) {
+          e.preventDefault();
+          $('.side-menu').addClass('contact');
+        });
+	$('.home').bind('click',function(e) {
+          e.preventDefault();
+          $('.side-menu').removeClass('contact');
+        });
+      });
+document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('contactme').addEventListener('click', function( e ) {
+            Custombox.open({
+                target: '.contact-form',
+                effect: 'fadein'
+            });
+            e.preventDefault();
+        });
+    });
+</script>
+<script type="text/javascript">
+$(window).load(function() {
+ $(".pageloaddiv").fadeOut(2000);
+});
+$(window).mousemove(function(event) {
+  $("#image").css({"left" : event.pageX, "top" : event.pageY});
+});
+</script>
+<div class="pageloaddiv">
+</div>
+  </body>
+<audio autoplay="true" loop src="songs/jinglebell.mp3"></audio>
+</html>
